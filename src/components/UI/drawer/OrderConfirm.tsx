927068dc9585c3cd34df24f247b0c2e3d2966ea7
David <David.Piruzashvili@gmail.com>
2025-01-23 22:27:48 +0400

    OrderConfirm component adding setTimeout for a smooth transition

--- src/components/UI/drawer/OrderConfirm.tsx
+++ src/components/UI/drawer/OrderConfirm.tsx
@@ -103,7 +103,9 @@ export const OrderConfirm = ({ open, setOpen }: Props) => {
                     style: { display: 'block', marginTop: '70px', width: '100%' },
                     onClick: () => {
                         setOpen(false);
-                        router.push('/contacts');
+                        setTimeout(() => {
+                            router.push('/contacts');
+                        }, 100)
                     },
                     disabled: !cartOrder.length
                 }}>
