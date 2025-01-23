b5fd349433c19b209c46162350bb113b1c302ef9
David <David.Piruzashvili@gmail.com>
2025-01-23 18:03:27 +0400

    @import google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout({ children }: LayoutProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro&display=optional" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       </head>
       <body className={`${museoSans.variable} ${museoMono.variable} ${museoBold.variable}`} id="body">
