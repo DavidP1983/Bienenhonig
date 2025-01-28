e6af873bf9b8d8eff50a0dc07dfbf6e438a52575
David <David.Piruzashvili@gmail.com>
2025-01-28 23:15:32 +0400

    layout.tsx  changes, added meta attributes maximum-scale=1.0, user-scalable=no

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import { Header } from "@/components/Header";
 import { MantineProvider } from '@mantine/core';
@@ -31,6 +31,13 @@ const museoBold = localFont({
   variable: '--font-museoBold'
 
 })
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 
 export const metadata: Metadata = {
