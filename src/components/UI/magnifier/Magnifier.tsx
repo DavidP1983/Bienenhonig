9bb76ebb5add35f036d2c8c8549074655bb90fe9
David <David.Piruzashvili@gmail.com>
2025-02-03 00:12:07 +0400

    Magnifier component bug fixed

--- src/components/UI/magnifier/Magnifier.tsx
+++ src/components/UI/magnifier/Magnifier.tsx
@@ -4,16 +4,19 @@ import Magnifier from "react18-image-magnifier";
 export const MagnifierImg = ({ imageSrc }: { imageSrc: string }) => {
     return (
         <Magnifier
-            src={imageSrc}
-            alt="image presentation"
-            mgShape="circle"
-            mgBorderWidth={2}
-            mgWidth={200}
-            mgHeight={200}
-            zoomFactor={1.5}
-            zoomImgSrc={imageSrc}
-            width="60%"
-            mgShowOverflow={true}
+            {...({
+                src: imageSrc,
+                zoomImgSrc: imageSrc,
+                alt: "image presentation",
+                mgShape: "circle",
+                mgBorderWidth: 2,
+                mgWidth: 200,
+                mgHeight: 200,
+                zoomFactor: 1.5,
+                width: "60%",
+                mgShowOverflow: true
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            } as any)}
         />
     )
 }
